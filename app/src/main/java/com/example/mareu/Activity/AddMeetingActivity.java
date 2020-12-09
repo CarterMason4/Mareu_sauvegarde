96aeedf3ebd07d0b558d352f2089188f42d655a1
CarterMason4 <nicotvas@hotmail.fr>
2020-12-09 21:56:29 +0100

    Développement de la responsiveness.

--- app/src/main/java/com/example/mareu/Activity/AddMeetingActivity.java
+++ app/src/main/java/com/example/mareu/Activity/AddMeetingActivity.java
@@ -1,23 +1,31 @@
 package com.example.mareu.Activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.DatePicker;
 import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.example.mareu.Api.MeetingApiService;
 import com.example.mareu.DI.Di;
@@ -39,43 +47,59 @@ import java.util.Locale;
 
 public class AddMeetingActivity extends AppCompatActivity{
 
-
+    @Nullable
     @BindView(R.id.date)
     TextView date;
 
+    @Nullable
     @BindView(R.id.heure)
     TextView heure;
 
+    @Nullable
     @BindView(R.id.editTextSalle)
     AutoCompleteTextView editTextSalle;
 
+    @Nullable
     @BindView(R.id.editTextViewIntervenants)
     MultiAutoCompleteTextView editTextIntervenants;
 
+    @Nullable
     @BindView(R.id.editTextAPropos)
     TextInputEditText editTextAPropos;
 
-    @BindView(R.id.meetingToolbar)
-    Toolbar toolbar;
+    @Nullable
+    @BindView(R.id.valider_button)
+    MaterialButton button;
 
     private MeetingApiService apiService;
+    private Toolbar toolbar;
 
     private int mHour;
     private int mMinutes;
+
+    private int orientation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_meeting);
 
-        setSupportActionBar(toolbar);
+         toolbar = findViewById(R.id.meetingToolbar);
 
-        ButterKnife.bind(this);
-        apiService = Di.getMeetingApiService();
+         setSupportActionBar(toolbar);
+
+        orientation = getResources().getConfiguration().orientation;
 
         if(getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+
+        makeToast("activité chargée !");
+
+        ButterKnife.bind(this);
+
+        apiService = Di.getMeetingApiService();
 
         setUpViews();
     }
@@ -108,11 +132,30 @@ public class AddMeetingActivity extends AppCompatActivity{
         editTextIntervenants.setAdapter(intervenantsAdapter);
 
         editTextIntervenants.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
-
-
     }
 
-    // Dans cette méthode, on implémente la logique de traitement des données.
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            getMenuInflater().inflate(R.menu.menu_ajoute_reunion_landscape, menu);
+            toolbar.setOverflowIcon(ContextCompat.getDrawable(this, R.drawable.ic_check));
+        } else {
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            toolbar.setOverflowIcon(ContextCompat.getDrawable(this, R.drawable.ic_check));
+        }
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //makeToast("J'ai cliqué !");
+        return super.onOptionsItemSelected(item);
+    }
 
     @OnClick(R.id.valider_button)
     public void valider() {
@@ -125,6 +168,12 @@ public class AddMeetingActivity extends AppCompatActivity{
 
 
         // apiService.addMeeting(new Reunion());
+
+
+        // Dès que le traitement est terminé
+        // On finit l'activité
+
+        finish();
     }
 
     @OnClick(R.id.date)
@@ -134,7 +183,6 @@ public class AddMeetingActivity extends AppCompatActivity{
             public void onDateChanged(@NonNull DatePicker view, int year, int month, int dayOfMonth) {
 
                 String currentDate = String.valueOf(dayOfMonth) + '/' + (month + 1) + '/' + year;
-
                 date.setText(currentDate);
             }
         };
@@ -160,5 +208,10 @@ public class AddMeetingActivity extends AppCompatActivity{
     }
 
 
+    private void makeToast(String string) {
+        Toast toast = Toast.makeText(this, string, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER_HORIZONTAL, 0, 0);
+        toast.show();
+    }
 
 }
